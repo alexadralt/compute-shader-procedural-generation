a9a1b852f3e5f7237b5079b2706254dfba776b6c
alexadralt <nikita.shevyrin@gmail.com>
2026-07-07 20:25:47 +0500

    small shader refactor

--- assets/shaders/compute/terrain_gen.hlsl
+++ assets/shaders/compute/terrain_gen.hlsl
@@ -27,14 +27,11 @@ float noise(float2 coords)
 {
     Terrain_Generation_Settings terrain_generation_settings = push_constants.terrain_gen_settings.Get();
     
-    float x = coords.x / (terrain_generation_settings.frequency * terrain_generation_settings.terrain_size);
-    float y = coords.y / (terrain_generation_settings.frequency * terrain_generation_settings.terrain_size);
+    coords /= (terrain_generation_settings.frequency * terrain_generation_settings.terrain_size);
+    coords -= 0.5;
     
-    x -= 0.5;
-    y -= 0.5;
-    
-    float value = smax(abs(x - y), abs(x + y), 0.05);
-    return value * 2.0 - 1.0;
+    float value = smax(abs(coords.x - coords.y), abs(coords.x + coords.y), 0.05);
+    return clamp(value, 0, 1) * 2.0 - 1.0;
 }
 
 [numthreads(8, 8, 1)]
@@ -49,7 +46,7 @@ void main(uint3 dispatch_thread_id : SV_DispatchThreadID)
     float noise_value = noise(float2(dispatch_thread_id.xy) * frequency);
     height_map[dispatch_thread_id.x * terrain_size + dispatch_thread_id.y] = noise_value * amplitude;
     
-    float scaled_noise_value = clamp((noise_value + 1.0) / 2.0, 0.0, 1.0);
+    float scaled_noise_value = (noise_value + 1.0) / 2.0;
     float4 final_color = float4(max(0.5 - scaled_noise_value, 0.0) * 2.0, 0, max(scaled_noise_value - 0.5, 0.0) * 2.0, 1);
-    height_map_image[uint2(dispatch_thread_id.x, dispatch_thread_id.y)] = final_color.bgra; // swizzle to match actual bgra8 format
+    height_map_image[uint2(dispatch_thread_id.x, dispatch_thread_id.y)] = final_color.bgra; // swizzle to match actual bgra layout
 }
