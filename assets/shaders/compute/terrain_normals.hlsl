b1b33ffc35081ab9107cc4113eaa583abc901307
alexadralt <nikita.shevyrin@gmail.com>
2026-07-16 01:10:19 +0500

    code formatting

--- assets/shaders/compute/terrain_normals.hlsl
+++ assets/shaders/compute/terrain_normals.hlsl
@@ -29,10 +29,10 @@ void main(uint3 dispatch_thread_id : SV_DispatchThreadID)
     uint y_plus_h_index  = map_index(chunk_coords + uint2(0, dispatch_thread_id.y + 1 >= terrain_size));
     uint y_minus_h_index = map_index(chunk_coords - uint2(0, dispatch_thread_id.y     == 0           ));
     
-    float x_plus_h  = height_map[x_plus_h_index ][((dispatch_thread_id.x + 1) & (terrain_size - 1)) * terrain_size + dispatch_thread_id.y    ];
-    float x_minus_h = height_map[x_minus_h_index][((dispatch_thread_id.x - 1) & (terrain_size - 1)) * terrain_size + dispatch_thread_id.y    ];
-    float y_plus_h  = height_map[y_plus_h_index ][ dispatch_thread_id.x                             * terrain_size + ((dispatch_thread_id.y + 1) & (terrain_size - 1))];
-    float y_minus_h = height_map[y_minus_h_index][ dispatch_thread_id.x                             * terrain_size + ((dispatch_thread_id.y - 1) & (terrain_size - 1))];
+    float x_plus_h  = height_map[x_plus_h_index ][((dispatch_thread_id.x + 1) & (terrain_size - 1)) * terrain_size +   dispatch_thread_id.y                           ];
+    float x_minus_h = height_map[x_minus_h_index][((dispatch_thread_id.x - 1) & (terrain_size - 1)) * terrain_size +   dispatch_thread_id.y                           ];
+    float y_plus_h  = height_map[y_plus_h_index ][  dispatch_thread_id.x                            * terrain_size + ((dispatch_thread_id.y + 1) & (terrain_size - 1))];
+    float y_minus_h = height_map[y_minus_h_index][  dispatch_thread_id.x                            * terrain_size + ((dispatch_thread_id.y - 1) & (terrain_size - 1))];
     float2 grad = normalize(float2(x_plus_h - x_minus_h, y_plus_h - y_minus_h)); // formally, here we should divide by 2*h, but beacuse we normalize it gets cancelled out anyway
     
     out_norm_gradient_map[dispatch_thread_id.z][dispatch_thread_id.x * terrain_size + dispatch_thread_id.y] = grad;
